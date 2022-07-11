ac3cddfffbcc1956a7803fc561defbb30eccd170
Suraj Tiwari <surajtiwari020@gmail.com>
2022-07-12 02:55:07 +0530

    added support for CUSTOM config dir path

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/spf13/viper"
+	"os"
 )
 
 func LoadConfig() error {
@@ -9,7 +10,10 @@ func LoadConfig() error {
 	viper.SetConfigType("yaml")            // REQUIRED if the config file does not have the extension in the name
 	viper.AddConfigPath(".")               // optionally look for config in the working directory
 	viper.AddConfigPath("/etc/api-config") // look for config in the api-config directory on the server
-
+	configPath := os.Getenv("CONFIG_PATH_DIR")
+	if configPath != "" {
+		viper.AddConfigPath(configPath)
+	}
 	if err := viper.ReadInConfig(); err != nil {
 		return err
 	}
