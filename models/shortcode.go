830905b04522d301beae6bbf4f751c3578023cae
Suraj Tiwari <surajtiwari020@gmail.com>
2022-07-12 08:21:39 +0530

    added support for new shortcode query, mutation and subscription

--- models/shortcode.go
+++ models/shortcode.go
@@ -1,11 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Shortcode struct {
 	gorm.Model
-	Code    string
-	Request string
-	UserID  uint
-	User    User
+	Code      string
+	Request   string
+	UserID    uint
+	User      User
+	CreatedOn time.Time `gorm:"autoCreateTime"`
 }
